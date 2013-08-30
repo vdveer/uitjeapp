e9728f07e4f6aa54828cee4cdd52c4617d704cb4
vdveer <wouter.van.der.veer@topicus.nl>
2013-08-30 12:24:21 +0200

    audio html5 test

--- audio.js
+++ audio.js
@@ -1,16 +1,8 @@
-var mediaplayer;
-var lastsong;
-function playsound(filename){
-	lastsong = filename;
-	if(mediaplayer != undefined){
-		mediaplayer.release();
-	}
-	mediaplayer = new Media(lastsong, function(){}, onAudioFail).play();
-}
 
-function onAudioFail(){
-	if(mediaplayer != undefined){
-		mediaplayer.release();
+function playsound(filename){
+	var a = document.getElementsByTagName("audio")[0];
+	if (a !== undefined){
+		a.src = filename;
+		a.play();
 	}
-	mediaplayer = new Media("/android_asset/www/"+lastsong, function(){}, onAudioFail).play();
 }
