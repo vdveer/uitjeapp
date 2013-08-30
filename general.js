a919b856b2e3515338e1eff57ef9de02f3219614
vdveer <wouter.van.der.veer@topicus.nl>
2013-08-30 18:18:14 +0200

    statusbar part5

--- general.js
+++ general.js
@@ -4,20 +4,36 @@ var downloaded = 0;
 if(window.applicationCache){
  	window.applicationCache.addEventListener("updateready", function(e){
  		document.getElementById("status").innerHTML = "Available offline";
+ 		if(!window.onLine){
+		document.getElementById("status").innerHTML = "Working offline now";
+	}
  	}, false);
  	window.applicationCache.addEventListener("cached", function(e){
  		document.getElementById("status").innerHTML = "Available offline";
+ 		if(!window.onLine){
+		document.getElementById("status").innerHTML = "Working offline now";
+	}
  	}, false);
  	window.applicationCache.addEventListener("checking", function(e){
  		document.getElementById("status").innerHTML = "Checking";
+ 		if(!window.onLine){
+		document.getElementById("status").innerHTML = "Working offline now";
+	}
  	}, false);
  	window.applicationCache.addEventListener("noupdate", function(e){
  		document.getElementById("status").innerHTML = "Available offline";
+ 		if(!window.onLine){
+		document.getElementById("status").innerHTML = "Working offline now";
+	}
  	}, false);
  	window.applicationCache.addEventListener("downloading", function(e){
  		downloaded++;
  		document.getElementById("status").innerHTML = "Downloading... [" + downloaded + "/" + totalFiles + "]"; 
+ 		if(!window.onLine){
+		document.getElementById("status").innerHTML = "Working offline now";
+	}
  	}, false);
+
 }else {
 	document.getElementById("status").innerHTML = "Not supported"
 }
