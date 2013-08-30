0d0811ad1263b2a17db6c4e6bc8e7c9ddb5cc9c8
vdveer <wouter.van.der.veer@topicus.nl>
2013-08-30 17:48:37 +0200

    statusbar

--- general.js
+++ general.js
@@ -1,36 +1,10 @@
-var CACHING = {
-
-doCacheAll: function(){
-
-	var element = document.getElementById('download');
-	element.setAttribute("onclick", "");
-	element.innerHTML = "Downloading... 0%";
-	
-	var myLoader = html5Preloader();
-
-	myLoader.on('finish', function(){ console.log('All assets loaded.'); });
-	myLoader.on('error', function(e){ console.error(e); });
-
-	myLoader.addFiles('activities.html',
-'audio.js',
-'config.xml',
-'contact.html',
-'general.js',
-'home.css',
-'hotel.html',
-'hotel.js',
-'icon.png',
-'index.html',
-'leaflet.css',
-'leaflet.ie.css',
-'leaflet.js',
-'map.html',
-'map.js',
-'polish.html',
-'restaurant.css',
-'restaurant.html');
-
+if(window.applicationCache){
+ 	window.applicationCache.onupdateready = function(){
+ 		document.getElementById("status").innerHTML = "Update downloaded";
+ 	};
+ 	window.applicationCache.noupdate = function(){
+ 		document.getElementById("status").innerHTML = "Up to date";
+ 	};
+}else {
+	document.getElementById("status").innerHTML = "Not supported"
 }
-
-
-};
