3d3f89af3036d8d5db475f43c07c1184b49891ae
vdveer <wouter.van.der.veer@topicus.nl>
2013-08-30 18:00:57 +0200

    statusbar part2

--- general.js
+++ general.js
@@ -1,10 +1,23 @@
+var totalFiles = 559;
+var downloaded = 0;
+
 if(window.applicationCache){
- 	window.applicationCache.onupdateready = function(){
- 		document.getElementById("status").innerHTML = "Update downloaded";
- 	};
- 	window.applicationCache.noupdate = function(){
- 		document.getElementById("status").innerHTML = "Up to date";
- 	};
+ 	window.applicationCache.addEventListener("updateready", function(e){
+ 		document.getElementById("status").innerHTML = "Available offline";
+ 	}, false);
+ 	window.applicationCache.addEventListener("cached", function(e){
+ 		document.getElementById("status").innerHTML = "Available offline";
+ 	}, false);
+ 	window.applicationCache.addEventListener("checking", function(e){
+ 		document.getElementById("status").innerHTML = "Checking";
+ 	}, false);
+ 	window.applicationCache.addEventListener("noupdate", function(e){
+ 		document.getElementById("status").innerHTML = "Available offline";
+ 	}, false);
+ 	window.applicationCache.addEventListener("downloading", function(e){
+ 		downloaded++;
+ 		document.getElementById("status").innerHTML = "Downloading... [" + downloaded + "/" + totalFiles + "]"; 
+ 	}, false);
 }else {
 	document.getElementById("status").innerHTML = "Not supported"
 }
